0b9a43083a1910357dc7c27590ce9db7b59a4ff2
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-06-27 16:13:54 -0400

    sketched out rough plan for clicking a section

--- js/app.js
+++ js/app.js
@@ -25,6 +25,10 @@
 const navBarMenu = document.getElementsByClassName('navbar__menu');
 const ul = document.getElementById('navbar__list');
 
+const createLi = document.createElement('li');
+const createAnchor = document.createElement('a');
+
+
 /**
  * End Global Variables
  * Start Helper Functions
@@ -40,13 +44,33 @@ const ul = document.getElementById('navbar__list');
 */
 
 // build the nav
-// ul.innerHTML = `
-//     <li><a href="">section 1</a></li>; 
-//     <li><a >section 2</a></li>; 
-//     <li><a >section 3</a></li>;`;
-// navBarMenu.append(navBarList);
-// document.body.appendChild(navBarMenus);
+ul.innerHTML = `
+    <li><a href="/" class= "menu__link">section 1</a></li>; 
+    //add a class or ID to each anchor using a for loop section[i]
+    <li><a href="/"  class= "menu__link section2">section 2</a></li>; 
+    <li><a href="/" class= "menu__link">section 3</a></li>;`;
+//navBarMenu.append(navBarList);
+//document.body.appendChild(navBarMenus);
 // Add class 'active' to section when near top of viewport
+
+// display the rect of section 2
+const section2 = document.querySelector("#section2");
+const rect = section2.getBoundingClientRect();
+
+function showSection (){
+    window.scroll(0,1320.59375);
+};
+
+function highlightSection (){
+    //change the css for when a section is clicked 
+};
+
+const navBarList = document.querySelector('.navbar__list, .section2')
+
+navBarList.addEventListener('click', function(){
+    showSection();
+});
+
 
 
 // Scroll to anchor ID using scrollTO event
