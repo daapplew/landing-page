92fbd6093f711fe69769bb63cbe0533fbd6a474b
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-07-06 08:55:49 -0400

    built nav & scrollIntoView for each section

--- js/app.js
+++ js/app.js
@@ -23,11 +23,8 @@
  * 
 */
 const navBarMenu = document.getElementsByClassName('navbar__menu');
-const ul = document.getElementById('navbar__list');
-
-const createLi = document.createElement('li');
-const createAnchor = document.createElement('a');
-
+const sections = ["Section 1", "Section 2", "Section 3", "Section 4"];
+const navBarList = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
@@ -44,32 +41,33 @@ const createAnchor = document.createElement('a');
 */
 
 // build the nav
-ul.innerHTML = `
-    <li><a href="/" class= "menu__link">section 1</a></li>; 
-    //add a class or ID to each anchor using a for loop section[i]
-    <li><a href="/"  class= "menu__link section2">section 2</a></li>; 
-    <li><a href="/" class= "menu__link">section 3</a></li>;`;
-//navBarMenu.append(navBarList);
-//document.body.appendChild(navBarMenus);
-// Add class 'active' to section when near top of viewport
+sections.forEach((section,i) => {
+/* anchor link  */
+const anchorLink = document.createElement('a');
+anchorLink.setAttribute("class",`menu__link section${i+1}`);
+anchorLink.setAttribute("data-link",`section${i+1}`);
+anchorLink.innerHTML = section;
+navBarList.appendChild(anchorLink);
 
-// display the rect of section 2
-const section2 = document.querySelector("#section2");
-const rect = section2.getBoundingClientRect();
+/* list */
+const list = document.createElement('li');
+list.appendChild(anchorLink);
+navBarList.appendChild(list);
 
-function showSection (){
-    window.scroll(0,1320.59375);
-};
-
-function highlightSection (){
-    //change the css for when a section is clicked 
-};
-
-const navBarList = document.querySelector('.navbar__list, .section2')
-
-navBarList.addEventListener('click', function(){
-    showSection();
 });
+
+const sectionLinks = document.querySelectorAll(".menu__link");
+sectionLinks.forEach((item) => {
+    item.addEventListener("click", ()=>{
+        const el = document.getElementById(item.getAttribute(`data-link`));
+        el.scrollIntoView({behavior:"smooth",block:"center"})
+    })
+
+})
+
+// navBarList.addEventListener('click', function(){
+//     showSection();
+// });
 
 
 
