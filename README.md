fe82266a69ce71a69bfaee76a4d497c968327317
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-07-08 14:29:52 -0400

    added readME and added styling to active class

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Landing Page Project
 
-## Table of Contents
+Manipulating the DOM exercise.
 
-* [Instructions](#instructions)
+- Programmatically builds navigation.
+- Scrolls to anchors from navigation.
+- Highlights section in viewport upon scrolling.
 
-## Instructions
+# Programming Languages
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+- HTML
+- CSS
+- JavaScript
 
-To get started, open `js/app.js` and start building out the app's functionality
+# Authour
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+Dwayne Applewhite
--- js/app.js
+++ js/app.js
@@ -13,62 +13,91 @@
  * 
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
+
 
 /**
  * Define Global Variables
  * 
 */
 const navBarMenu = document.getElementsByClassName('navbar__menu');
-const sections = ["Section 1", "Section 2", "Section 3", "Section 4"];
-const navBarList = document.getElementById('navbar__list');
+const sections = ["Section 1", "Section 2", "Section 3", "Section 4"];// array of section items 
+const sectionElements = document.querySelectorAll("section"); // list of section tags
+const navBarList = document.getElementById('navbar__list'); //create variable for unorder list id
 
 /**
  * End Global Variables
- * Start Helper Functions
- * 
-*/
 
-
-
-/**
- * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
+
+/**
+ * build the nav
+ * for every item in the sections array create an <a>
+ * set class attribute to menu__link this will add some style
+ * set data-link attribute to sections so they can be 
+*/
 sections.forEach((section,i) => {
 /* anchor link  */
 const anchorLink = document.createElement('a');
 anchorLink.setAttribute("class",`menu__link section${i+1}`);
 anchorLink.setAttribute("data-link",`section${i+1}`);
 anchorLink.innerHTML = section;
+anchorLink.href = `#section${i+1}`;
 navBarList.appendChild(anchorLink);
 
 /* list */
 const list = document.createElement('li');
+list.setAttribute("class", "navbar__menu")
 list.appendChild(anchorLink);
 navBarList.appendChild(list);
 
 });
 
 const sectionLinks = document.querySelectorAll(".menu__link");
+
 sectionLinks.forEach((item) => {
     item.addEventListener("click", ()=>{
         const el = document.getElementById(item.getAttribute(`data-link`));
         el.scrollIntoView({behavior:"smooth",block:"center"})
     })
 
-})
+});
 
-// navBarList.addEventListener('click', function(){
-//     showSection();
-// });
+function checkView(section) {
+    let rectView = section.getBoundingClientRect();
+    console.log(sectionView);
+    return (rectView.top >= 0 && rectView.bottom < window.innerHeight);
 
+};
+
+
+function changeActiveClass() {
+    for (sectionElement of sectionElements) {
+        if (checkView(sectionElement)) {
+            //change nav background based on view
+            const sectionLinkName = sectionElement.getAttribute('data-nav'); 
+            for(const sectionLink of sectionLinks){
+                if(sectionLinkName === sectionLink.innerHTML){
+                    sectionLink.style = 'background-color: #333;';
+                }
+                else{
+                    sectionLink.style = 'background-color: #fff;'
+                }
+            }
+            //change section background style based on view
+            if (!sectionElement.classList.contains('your-active-class')) {
+                sectionElement.classList.add('your-active-class');
+            }
+        }
+        else {
+            sectionElement.classList.remove('your-active-class');
+        }
+    }
+};
+
+document.addEventListener('scroll', changeActiveClass);
 
 
 // Scroll to anchor ID using scrollTO event
@@ -85,5 +114,6 @@ sectionLinks.forEach((item) => {
 // Scroll to section on link click
 
 // Set sections as active
+
 
 
