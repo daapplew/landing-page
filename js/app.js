51cd311f9631c972dac1ad33ace4a49c1aca1686
Dwayne Applewhite <dwayneaapplewhite@gmail.com>
2022-07-09 07:54:11 -0400

    updated comments in JS

--- js/app.js
+++ js/app.js
@@ -17,12 +17,13 @@
 
 /**
  * Define Global Variables
- * 
+ * array of section items
+ * list of section tags
+ * create variable for unorder list id
 */
-const navBarMenu = document.getElementsByClassName('navbar__menu');
-const sections = ["Section 1", "Section 2", "Section 3", "Section 4"];// array of section items 
-const sectionElements = document.querySelectorAll("section"); // list of section tags
-const navBarList = document.getElementById('navbar__list'); //create variable for unorder list id
+const sections = ["Section 1", "Section 2", "Section 3", "Section 4"];
+const sectionElements = document.querySelectorAll("section"); 
+const navBarList = document.getElementById('navbar__list'); 
 
 /**
  * End Global Variables
@@ -34,45 +35,69 @@ const navBarList = document.getElementById('navbar__list'); //create variable fo
 
 /**
  * build the nav
- * for every item in the sections array create an <a>
+ * using a forEach method we will loop through an array and create elements, set classes, add text and append to document
+ * for every item in the sections array create an <a> & <li>
  * set class attribute to menu__link this will add some style
- * set data-link attribute to sections so they can be 
+ * set data-link attribute to designated sections 
+ * set class attribute for navbar menu that will add styling 
 */
 sections.forEach((section,i) => {
 /* anchor link  */
 const anchorLink = document.createElement('a');
 anchorLink.setAttribute("class",`menu__link section${i+1}`);
 anchorLink.setAttribute("data-link",`section${i+1}`);
-anchorLink.innerHTML = section;
-anchorLink.href = `#section${i+1}`;
-navBarList.appendChild(anchorLink);
+anchorLink.innerHTML = section; // add text inside each <a>
+anchorLink.href = `#section${i+1}`; //add href attribute 
+navBarList.appendChild(anchorLink); //append anchor to ul
 
 /* list */
 const list = document.createElement('li');
-list.setAttribute("class", "navbar__menu")
-list.appendChild(anchorLink);
-navBarList.appendChild(list);
+list.setAttribute("class", "navbar__menu");
+list.appendChild(anchorLink); //append anchor links to li
+navBarList.appendChild(list); // append everything to ul 
 
 });
 
-const sectionLinks = document.querySelectorAll(".menu__link");
 
+
+
+
+/**
+ * Scroll to section on link click
+ * Because we set menu link as a class in the anchor tags we can use querySelectorAll to select each item 
+ * we can assign each class item and get the data link attribute and assign that to a variable
+ * so using a forEach method we can add an event listener for each click with its designated section
+ * when we scroll into view make sure the behaviour is smooth and in center 
+ * prevent default method to get rid of default settings on click function
+*/
+const sectionLinks = document.querySelectorAll(".menu__link");
 sectionLinks.forEach((item) => {
     item.addEventListener("click", ()=>{
+    preventDefault();
         const el = document.getElementById(item.getAttribute(`data-link`));
         el.scrollIntoView({behavior:"smooth",block:"center"})
     })
-
 });
+
+/**
+ * function that checks the view and takes in one parameter called section 
+ * the top and bottom rectangle of each section will be compared to the window height
+ * this is a helper functon to change the active class 
+ */
 
 function checkView(section) {
     let rectView = section.getBoundingClientRect();
-    console.log(sectionView);
     return (rectView.top >= 0 && rectView.bottom < window.innerHeight);
 
 };
 
-
+/**
+ * function for active class
+ * for of loop that goes through each section tag
+ * checks the view of each section tag
+ * add styles
+ * add and remove active class using an if statement 
+ */
 function changeActiveClass() {
     for (sectionElement of sectionElements) {
         if (checkView(sectionElement)) {
@@ -97,10 +122,8 @@ function changeActiveClass() {
     }
 };
 
+//listens for scroll to call function that checks views and changes active class
 document.addEventListener('scroll', changeActiveClass);
-
-
-// Scroll to anchor ID using scrollTO event
 
 
 /**
@@ -109,11 +132,7 @@ document.addEventListener('scroll', changeActiveClass);
  * 
 */
 
-// Build menu 
 
-// Scroll to section on link click
-
-// Set sections as active
 
 
 
